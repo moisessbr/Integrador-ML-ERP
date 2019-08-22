576949f4cba12d2c1b43eb8ae7cf7e34abe8ef2a
moisessbr <moisess@outlook.com>
2019-08-22 14:53:58 -0300

    Arquivos atualizados

--- adendos22/updator.php
+++
@@ -1,9 +0,0 @@
-<?
-$link10 = mysqli_connect("mysql857.umbler.com","2ahanupr","?2{*}26ObouSh","fr8phust");
-
-mysqli_query($link10,"UPDATE melinotif SET status='novo' WHERE resource LIKE '/orders/%' LIMIT 50");
-
-mysqli_query($link10,"UPDATE melinotif SET status='novo' WHERE resource LIKE '/shipments/%' LIMIT 50");
-
-mysqli_close($link10);
-?>
